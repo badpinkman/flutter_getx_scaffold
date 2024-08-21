3a574eb71aa515e400a6e2c8a4bf6521451fd6b6
jesse <genghaifei.ghf@gmail.com>
2024-08-21 16:15:47 +0800

    画笔1

--- example/lib/pages/draw/widgets/paper_widget.dart
+++ example/lib/pages/draw/widgets/paper_widget.dart
@@ -20,10 +20,35 @@ class PaperPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     // 创建画笔
     final Paint paint = Paint();
-    // 绘制圆
-    canvas.drawCircle(const Offset(100, 100), 10, paint);
+    paint
+      ..color = Colors.blue //颜色
+      ..strokeWidth = 4 //线宽
+      ..style = PaintingStyle.stroke; //模式--线型
+
+    //绘制线
+    canvas.drawLine(const Offset(0, 0), const Offset(100, 100), paint);
+
+    Path path = Path();
+    path.moveTo(100, 100);
+    path.lineTo(200, 0);
+    canvas.drawPath(path, paint..color = Colors.red);
   }
+
+  void drawLine(Canvas canvas) {}
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
+
+// class PaperPainter extends CustomPainter {
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     // 创建画笔
+//     final Paint paint = Paint();
+//     // 绘制圆
+//     canvas.drawCircle(const Offset(100, 100), 10, paint);
+//   }
+//
+//   @override
+//   bool shouldRepaint(CustomPainter oldDelegate) => false;
+// }
